2ce249e03d8d63ba493e2eb7ff2a1f04f11e19d4
Adam <amarinelli@esri.ca>
2015-03-17 19:37:42 -0400

    Porting to use express

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
-var http = require('http');
-var url = require('url');
+// var http = require('http');
+// var url = require('url');
+// var request = require('request');
+//
+// http.createServer(function (req, res) {
+//     var queryObject = url.parse(req.url,true).query;
+//     var baseUrl = "http://home.esricanada.com/report/SCMCase.asp";
+//     var caseNumb = queryObject.text;
+//     var channel = queryObject.channel_name;
+//     console.log('CASE NUMBER:' + caseNumb);
+//     console.log('CHANNEL:' + channel);
+//     var returnUrl = baseUrl + "?Case_No=" + caseNumb;
+//     var caseUrl = "<" + returnUrl + "|Case#" + caseNumb + ">"
+//     var payload = {
+//         text: caseUrl,
+//         channel : channel
+//     };
+//     var userString = JSON.stringify(payload);
+//
+//     request.post({
+//       url:'https://hooks.slack.com/services/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',
+//       form: userString},
+//       function(err,httpResponse,body){
+//         console.log(err);
+//       });
+//
+// }).listen(process.env.PORT, process.env.IP);
+
+
 var request = require('request');
+var app = require('express');
+var bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-    var queryObject = url.parse(req.url,true).query;
-    var baseUrl = "http://home.esricanada.com/report/SCMCase.asp";
-    var caseNumb = queryObject.text;
-    var channel = queryObject.channel_name;
-    console.log('CASE NUMBER:' + caseNumb);
-    console.log('CHANNEL:' + channel);
-    var returnUrl = baseUrl + "?Case_No=" + caseNumb;
-    var caseUrl = "<" + returnUrl + "|Case#" + caseNumb + ">"
-    var payload = {
-        text: caseUrl,
-        channel : channel
-    };
-    var userString = JSON.stringify(payload);
+app.use(bodyParser.json()); // for parsing application/json
+app.set('port', (process.env.PORT || 5000));
 
-    request.post({
-      url:'https://hooks.slack.com/services/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',
-      form: userString},
-      function(err,httpResponse,body){
-        console.log(err);
-      });   
+app.post('/', function(req, res) {
+  res.send('Hello World!');
+  console.log(req.body);
+});
 
-}).listen(process.env.PORT, process.env.IP);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});

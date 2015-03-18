91e7cff765b3273875b2a4179daa2af3fc45ec9c
Adam <amarinelli@esri.ca>
2015-03-17 20:00:13 -0400

    Added parameters and variables for request

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// var http = require('http');
-// var url = require('url');
-// var request = require('request');
-//
-// http.createServer(function (req, res) {
-//     var queryObject = url.parse(req.url,true).query;
-//     var baseUrl = "http://home.esricanada.com/report/SCMCase.asp";
-//     var caseNumb = queryObject.text;
-//     var channel = queryObject.channel_name;
-//     console.log('CASE NUMBER:' + caseNumb);
-//     console.log('CHANNEL:' + channel);
-//     var returnUrl = baseUrl + "?Case_No=" + caseNumb;
-//     var caseUrl = "<" + returnUrl + "|Case#" + caseNumb + ">"
-//     var payload = {
-//         text: caseUrl,
-//         channel : channel
-//     };
-//     var userString = JSON.stringify(payload);
-//
-//     request.post({
-//       url:'https://hooks.slack.com/services/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',
-//       form: userString},
-//       function(err,httpResponse,body){
-//         console.log(err);
-//       });
-//
-// }).listen(process.env.PORT, process.env.IP);
-
-
 var request = require('request');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -36,9 +7,31 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.set('port', (process.env.PORT || 5000));
 
+//variables
+var baseUrl = "http://home.esricanada.com/report/SCMCase.asp";
+
 app.post('/', function(req, res) {
   console.log(req.body);
-  res.send('Hello World!');
+
+  var number = req.body.text;
+  var channel = req.body.channel_name;
+  var returnUrl = baseUrl + "?Case_No=" + caseNumb;
+  var caseUrl = "<" + returnUrl + "|Case#" + caseNumb + ">"
+
+  var payload = {
+      text: caseUrl,
+      channel : channel
+  };
+  var userString = JSON.stringify(payload);
+
+  request.post({
+    url:'https://hooks.slack.com/services/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',
+    form: userString},
+    function(err,httpResponse,body){
+      console.log(err);
+    });
+
+    res.end();
 });
 
 app.listen(app.get('port'), function() {
